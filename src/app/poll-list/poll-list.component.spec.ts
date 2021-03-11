42d94d11abfcb6cafdaa0d118654daf03adc2625
fulva1230 <b10503137@gapps.ntust.edu.tw>
2021-03-11 22:40:16 +0800

    make the test suite better

--- src/app/poll-list/poll-list.component.spec.ts
+++ src/app/poll-list/poll-list.component.spec.ts
@@ -2,17 +2,26 @@ import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {PollListComponent} from './poll-list.component';
 import {PollListService} from '../cloudservices/poll-list.service';
-import {mockPollList} from '../../test/service-mock';
+import {mockPollList, mockPollPullerAndPoster} from '../../test/service-mock';
+import {PollboxComponent} from '../pollbox/pollbox.component';
+import {PollPullerService} from '../cloudservices/poll-puller.service';
+import {PollPosterService} from '../cloudservices/poll-poster.service';
 
 describe('PollListComponent', () => {
   let component: PollListComponent;
   let fixture: ComponentFixture<PollListComponent>;
 
   beforeEach(async () => {
+    const {pollPullerSpy, pollPosterSpy} = mockPollPullerAndPoster();
     await TestBed.configureTestingModule({
-      declarations: [PollListComponent],
+      declarations: [
+        PollListComponent,
+        PollboxComponent
+      ],
       providers: [
-        {provide: PollListService, useValue: mockPollList()}
+        {provide: PollListService, useValue: mockPollList()},
+        {provide: PollPullerService, useValue: pollPullerSpy},
+        {provide: PollPosterService, useValue: pollPosterSpy},
       ]
     })
       .compileComponents();
@@ -21,6 +30,7 @@ describe('PollListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PollListComponent);
     component = fixture.componentInstance;
+    component.applierName = 'stone';
     fixture.detectChanges();
   });
 
@@ -30,5 +40,22 @@ describe('PollListComponent', () => {
 
   it('should get poll list', () => {
     expect(component.questionIds.length).toBe(4);
+  });
+
+  it('should have 4 children components', () => {
+    const debugElement = fixture.debugElement;
+    expect(debugElement.children.length).toBe(4);
+  });
+
+  it('children components have appropriate data binding', () => {
+    const debugElement = fixture.debugElement;
+    for (const childPollBox of debugElement.children) {
+      const pollboxInst: PollboxComponent | undefined = childPollBox.componentInstance;
+      expect(pollboxInst).toBeTruthy();
+      if (pollboxInst) {
+        expect(pollboxInst.applierName).toBe('stone');
+        expect(pollboxInst.pollbox).toBeTruthy();
+      }
+    }
   });
 });
