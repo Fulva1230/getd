1df8f39ae65b331763ba71e207f88f61420382f9
fulva1230 <b10503137@gapps.ntust.edu.tw>
2021-04-25 21:08:34 +0800

    write a trivial test to know the way subject works

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,18 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import {TestBed} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {AppComponent} from './app.component';
+import {Subject} from 'rxjs';
 
 describe('AppComponent', () => {
+  it('test subject', () => {
+    const subject = new Subject();
+    let i = 0;
+    subject.complete();
+    subject.subscribe(val => {
+    }, err => {
+    }, () => {
+      i = 1;
+    });
+    expect(i).toEqual(1);
+  });
 });
